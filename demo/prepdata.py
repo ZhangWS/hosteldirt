d32b6b301f9e9d2dbd00f140aa2775d3a87deb0f
Wenshuo Zhang <11928078+ZhangWS@users.noreply.github.com>
2018-10-16 01:55:43 -0700

    Update demo/prepdata.py

--- demo/prepdata.py
+++ demo/prepdata.py
@@ -1,20 +1,30 @@
 from sqlalchemy import create_engine, Table, MetaData
 from sqlalchemy_utils import *
+import psycopg2
 import pandas as pd
 
 
 #Read in available data. Currently from CSV, but will be modified to a PostgreSQL db
 def readdata():
 
-##This may need to be changed in case we want to work with a SQL db instead
-##check on how fast SQL queries are versus from pandas
+##this data is updated via code from Python notebooks and should probably be executed once in a while for maximum fidelity
+    db = 'hostel'
+    username='Natalia'
+    engine = create_engine('postgres://%s@localhost/%s'%(username,db))
+    q = text('SELECT * from hostellinks')
+    hostellinks = pd.DataFrame(engine.execute(q).fetchall())
+#    hostellinks = pd.read_csv('data/hostellinks.csv')
+#    hostelreviews = pd.read_csv('data/hostelreviews.csv')
+    q=text('SELECT * FROM hostelreviews')
+    hostelreviews = pd.DataFrame(engine.execute(q).fetchall())
 
-    hostellinks = pd.read_csv('data/hostellinks.csv')
-    hostelreviews = pd.read_csv('data/hostelreviews.csv')
 
     #there are rows where one field is NA because of formatting
     #get rid of them or else training will pitch fits
-    cleanlabeled = pd.read_csv('data/cleanlabeled.csv')
+#    cleanlabeled = pd.read_csv('data/cleanlabeled.csv')
+    q = text('SELECT * from cleanlabeled')
+    cleanlabeled = pd.DataFrame(engine.execute(q).fetchall())
+
     cleanlabeled = cleanlabeled.dropna(axis = 0)
 
     return hostellinks, hostelreviews, cleanlabeled
